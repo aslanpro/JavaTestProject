5783cc4aa4c9d9586758fed730d436e28ec274c5
Oleksandr Derkach <alex7derkach@gmail.com>
2014-05-17 14:18:06 +0300

    Extension changed

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class JavaTestProject
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hello GitHub!");
+	}
+}
