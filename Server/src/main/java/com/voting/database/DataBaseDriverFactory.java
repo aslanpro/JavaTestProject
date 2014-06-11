52bf7cd5d1240ac03870afe8c601bc62704433a3
aslanpro <aslan@torba.com>
2014-06-11 03:38:24 +0300

    without java 1.7 file reading

--- Server/src/main/java/com/voting/database/DataBaseDriverFactory.java
+++ Server/src/main/java/com/voting/database/DataBaseDriverFactory.java
@@ -1,23 +1,16 @@
 package com.voting.database;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import com.voting.database.nosql.NoSqlConnector;
 
 public class DataBaseDriverFactory {
 	static public DataBaseDriver getDriver(String path) {
-		Charset charset = Charset.forName("US-ASCII");
-
-		Path file = FileSystems.getDefault().getPath(path);
-
 		BufferedReader reader = null;
 		try {
-			reader = Files.newBufferedReader(file, charset);
+			reader = new BufferedReader(new FileReader(path));
 			String line = reader.readLine();
 
 			if (line.equals("nosql")) {
