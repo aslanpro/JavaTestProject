5a2cadbc87f4b4d021b1ab1427d206fdbe4f9b8c
Iryna Kushnir <rinakushnir@mail.ru>
2014-06-11 03:48:06 +0300

    Adding bin folder

---
+++ VotingSystemTests/bin/classes.dex.d
@@ -0,0 +1,10 @@
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes.dex : \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\tests\KeyRegistrationTests.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\tests\VotingCreationTests.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\tests\VotingTests.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\utils\AppActions.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\utils\ConnectToServerActivity.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\utils\CreateVotingDialog.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\utils\RegisterKeyDialog.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\utils\VoteActivity.class \
+D:\gitrepo\JavaTestProject\VotingSystemTests\bin\classes\utils\VotingDialog.class \
